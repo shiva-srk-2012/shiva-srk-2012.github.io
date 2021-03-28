3c0ae3c582af5e1bd72c39f65a9a3da4a247cb45
shiva-srk-2012 <80212865+shiva-srk-2012@users.noreply.github.com>
2021-03-28 21:33:22 +0530

    website changes

---
+++ precache-manifest.3201a3c5c8e826599df251bec450c846.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "00bc802896518376b77e",
+    "url": "/static/css/main.88229b4b.chunk.css"
+  },
+  {
+    "revision": "00bc802896518376b77e",
+    "url": "/static/js/main.00bc8028.chunk.js"
+  },
+  {
+    "revision": "825a4ebb3e54f27bf772",
+    "url": "/static/css/1.3deaec50.chunk.css"
+  },
+  {
+    "revision": "825a4ebb3e54f27bf772",
+    "url": "/static/js/1.825a4ebb.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8041c112e8539f70e699d9465376f4d5",
+    "url": "/index.html"
+  }
+];
